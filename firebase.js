79ad2a645c1a77a21e7f6d28d5957f2f2cf67607
majaffi <affiesson3@gmail.com>
2026-02-21 11:26:45 +0100

    Refactor Firebase imports and update config
    Updated Firebase SDK imports and configuration.

--- firebase.js
+++ firebase.js
@@ -1,17 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "PASTE_YOURS",
-  authDomain: "PASTE_YOURS",
-  projectId: "PASTE_YOURS",
-  storageBucket: "PASTE_YOURS",
-  messagingSenderId: "PASTE_YOURS",
-  appId: "PASTE_YOURS"
+  apiKey: "AIzaSyB1WGfGyzG8nCbiRqKwLbmAqHiFuxf5QyY",
+  authDomain: "majaffi-platform.firebaseapp.com",
+  projectId: "majaffi-platform",
+  storageBucket: "majaffi-platform.firebasestorage.app",
+  messagingSenderId: "1044333099678",
+  appId: "1:1044333099678:web:7a4bee30339f7d7033598f",
+  measurementId: "G-4087HL56RF"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
